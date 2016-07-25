d8c91d8c7630efdc00d2bb090450dd00d06d5d4d
Mr.Chen <dd@ddamy.com>
2016-07-25 12:12:24 +0800

    添加删除设置默认收货地址ajax

--- js/user/shipingAddress.js
+++ js/user/shipingAddress.js
@@ -5,7 +5,9 @@
 
 (function () {
     var addLeft = $('.addressbottomleft').toArray();
+    var lis = $('.address ul li').toArray();
     var del = $('.del').toArray();
+
     addLeft.forEach(function (v, i) {
         touch.on(v, 'tap', function () {
             var flag, cls, htm;
@@ -21,12 +23,23 @@
             cls = eval(flag).cls;
             htm = eval(flag).htm;
             test(v, cls, htm);
+
+            //执行添加默认
+            updateAddressState($(lis[i]).children('.flag').html());
+
         })
     })
 
     del.forEach(function(v,i){
         touch.on(v,'tap',function(){
-            $(v).parent().parent().parent().remove();
+            var parLi =  $(v).parent().parent().parent(),
+                parLiId = parLi.children('span').html();
+
+            parLi.remove();
+
+            //执行删除请求
+            deleteAddres(parLiId);
+
         })
     })
     function eval(flag) {
@@ -42,5 +55,56 @@
         v.querySelector('i').className = cls;
         v.querySelector('span').innerHTML = htm;
     }
+
+    //删除收货地址
+    function deleteAddres(id){
+        if(id != null && id != ""){
+            jQuery.ajax({
+                type : "POST",
+                url : "${ctx}/address/deleteAddress.html",
+                dataType : "json",
+                data : {'id':id},
+                cache : false,
+                contentType : "application/x-www-form-urlencoded; charset=utf-8",
+                success : function(data) {
+                    if(data.result){
+                        window.location.reload();
+                        return true;
+                    }
+                },
+                error : function(obj) {
+                    alert("服务器异常!!!");
+                    return false;
+                }
+            });
+        }else{
+            alert("服务器异常！");
+        }
+    }
+
+
+    //更新默认收货地址
+    function updateAddressState(id){
+        jQuery.ajax({
+            type : "POST",
+            url : "${ctx}/address/updateAddressState.html",
+            dataType : "json",
+            data : {"id":id},
+            cache : false,
+            contentType : "application/x-www-form-urlencoded; charset=utf-8",
+            success : function(data) {
+                if(data.result){
+                    window.location.reload();
+                    return true;
+                }
+            },
+            error : function(obj) {
+                alert("服务器异常!!!");
+                return false;
+            }
+        });
+    }
+
+
 
 })()
