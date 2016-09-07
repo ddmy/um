c3b032be7f36708102a493c3adaeeef4ff8561b7
Mr.Chen <dd@ddamy.com>
2016-09-07 09:57:10 +0800

    添加忘记密码页面

--- js/urmro.js
+++ js/urmro.js
@@ -29,20 +29,20 @@ urmro = (function () {
         }
     }
     if (!urmro.mobile) {
-        window.location.href = 'http://www.urmro.com';
+        //window.location.href = 'http://www.urmro.com';
     }
 
 
     // 对浏览器的UserAgent进行正则匹配，不含有微信独有标识的则为其他浏览器
     urmro.useragent = navigator.userAgent;
     if (urmro.useragent.match(/MicroMessenger/i) != 'MicroMessenger') {
-        document.body.innerHTML = "";
+       // document.body.innerHTML = "";
         // 这里警告框会阻塞当前页面继续加载
-        alert('已禁止本次访问：您必须使用微信内置浏览器访问本页面！');
+        //alert('已禁止本次访问：您必须使用微信内置浏览器访问本页面！');
         // 以下代码是用javascript强行关闭当前页面
-        var opened = window.open('about:blank', '_self');
-        opened.opener = null;
-        opened.close();
+        //var opened = window.open('about:blank', '_self');
+        //opened.opener = null;
+        //opened.close();
     }
 
 
